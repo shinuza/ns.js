e679e899ac10ad8bca4773c50edda223f7261723
Samori Gorse <sgorse@lefigaro.fr>
2013-10-11 16:28:03 +0200

    Refactoring

--- namespace.js
+++ namespace.js
@@ -1,16 +1,14 @@
-!(function(root) {
+;(function(root) {
 
   function ns(path, fn) {
     var i = 0
-      , isLast
       , parent = root
       , components = path.split('.')
       , len = components.length;
 
     for(;i < len; i+= 1) {
-      isLast = i === len - 1;
       if(!parent[components[i]]) {
-        parent[components[i]] = isLast ? fn() : {};
+        parent[components[i]] = i === len - 1 ? fn() : {};
       }
       parent = parent[components[i]];
     }
